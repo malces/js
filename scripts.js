d4eb6bba2059115689b6cec01a22b65e5644926c
malces <cesar.mallim@gmail.com>
2016-03-20 15:17:28 -0500

    inicio de java scripts

---
+++ scripts.js
@@ -0,0 +1,9 @@
+console.log("Inicio de programa");
+
+alert("Hola JavaScript");
+
+document.write("<h1> Hola JavaScript!!!</h1>");
+
+alert("gracias por su visita");
+
+console.log("Fin del programa");
