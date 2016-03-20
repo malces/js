fb35a05a85bf65dc362c9015f7f0c7d890d787ab
malces <cesar.mallim@gmail.com>
2016-03-20 17:25:43 -0500

    asignacion a variables

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ console.log("Inicio de programa"):
 alert("Hola JavaScript");
 document.write("<h1> Hola JavaScript!!!</h1>");
 alert("gracias por su visita"");
+
 console.l0g("Fin del programa");
 var equipo= prompt("Cual es tu equipo favorito ??");
 console.log ("El equipo es:" + equipo);
