443ee2307d057206998f3840bb8af3779ac46285
malces <cesar.mallim@gmail.com>
2016-03-20 15:41:34 -0500

    comienzo de error

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-console.log("Inicio de programa");
+console.log("Inicio de programa"):
 
 alert("Hola JavaScript");
 
 document.write("<h1> Hola JavaScript!!!</h1>");
 
-alert("gracias por su visita");
+alert("gracias por su visita"");
 
-console.log("Fin del programa");
+console.l0g("Fin del programa");
