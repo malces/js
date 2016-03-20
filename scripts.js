b56e88f1d1ed04c2f8d6e1b925794673d4feaf29
malces <cesar.mallim@gmail.com>
2016-03-20 17:22:38 -0500

    asignacion a variables

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
-/*console.log("Inicio de programa"):
-
+console.log("Inicio de programa"):
 alert("Hola JavaScript");
-
 document.write("<h1> Hola JavaScript!!!</h1>");
-
 alert("gracias por su visita"");
-
-console.l0g("Fin del programa");*/
-
+console.l0g("Fin del programa");
 var equipo= prompt("Cual es tu equipo favorito ??");
 console.log ("El equipo es:" + equipo);
+
+var clave = prompt("Hola Usuario, Ingrese su clave!");
+console.log("La longitud de la clave es:" + clave.length);
+
+var clave = prompt("Hola Usuario, Ingrese su clave ! ");
+console.log("La longitud de la clave:" + clave.length);
+console.log("TA-DA!!! su clave es:" + clave.toUpperCase());
+console.log("El valor original de la clave es: " + clave);
+
+document.write("<h1> Hola JavaScript!!!</h1>" clave );
