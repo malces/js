72780940d724a2c908a41cc42088eb6f9777f79d
malces <cesar.mallim@gmail.com>
2016-03-25 21:26:05 -0500

    revisando clases de js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-console.log("Inicio de programa"):
+console.log("Inicio de programa");
 alert("Hola JavaScript");
 document.write("<h1> Hola JavaScript!!!</h1>");
-alert("gracias por su visita"");
-console.l0g("Fin del programa");
+alert("gracias por su visita");
+console.log("Fin del programa");
+
 var equipo= prompt("Cual es tu equipo favorito ??");
 console.log ("El equipo es:" + equipo);
 
@@ -14,4 +15,4 @@ console.log("La longitud de la clave:" + clave.length);
 console.log("TA-DA!!! su clave es:" + clave.toUpperCase());
 console.log("El valor original de la clave es: " + clave);
 
-document.write("<h1> Hola JavaScript!!!</h1>" clave );
+document.write("<h2> SU CLAVE ES: " + clave + "</h2>");
