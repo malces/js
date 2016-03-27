31148b7021dcd8979e4fcd70d4e576c7423e029c
malces <cesar.mallim@gmail.com>
2016-03-27 09:20:29 -0500

    funcion parseInt

--- scripts.js
+++ scripts.js
@@ -1,18 +1,47 @@
-console.log("Inicio de programa"):
+/*console.log("Inicio de programa");
 alert("Hola JavaScript");
 document.write("<h1> Hola JavaScript!!!</h1>");
-alert("gracias por su visita"");
-
-console.l0g("Fin del programa");
+alert("gracias por su visita");
+console.log("Fin del programa");
 var equipo= prompt("Cual es tu equipo favorito ??");
 console.log ("El equipo es:" + equipo);
-
 var clave = prompt("Hola Usuario, Ingrese su clave!");
 console.log("La longitud de la clave es:" + clave.length);
-
 var clave = prompt("Hola Usuario, Ingrese su clave ! ");
 console.log("La longitud de la clave:" + clave.length);
 console.log("TA-DA!!! su clave es:" + clave.toUpperCase());
 console.log("El valor original de la clave es: " + clave);
+document.write("<h2> SU CLAVE ES: " + clave + "</h2>");*/
 
-document.write("<h1> Hola JavaScript!!!</h1>" clave );
+/*console.log("Inicio del Programa");
+document.write("<h1> CUENTO MUY CORTITO </h1>");
+document.write("<h2> Ingrese los siguientes datos </h2>");
+
+var sustantivo = prompt("Ingrese el sustantivo");
+var adjetivo = prompt("Ingrese el adjetivo");
+var verbo = prompt("Ingrese el verbo");
+
+// PALABRAS PARA INGRESAR 
+// SUSTANTIVO: mujer
+// ABJETIVO: enamorada
+// VERBO: conquistar 
+
+document.write("<h3> La " + sustantivo + " americana " + adjetivo + " quiere un perrito para " + verbo + " al novio </h3>");
+document.write("<h3> El novio " + verbo + " quiere una " + sustantivo + " americana y " + adjetivo + " de un perrito </h3>");
+document.write("<h3> El perrito " + adjetivo + " quiero un novio para " + verbo + " a la " + sustantivo + " americana </h3>");
+document.write("<h3> Y VIVIERON FELICES... </h3>");*/
+
+
+/*var segundosPorMinuto = 60;
+var minutosPorHora= 60;
+var horasPorDia =24;
+var segundosPorDia = segundosPorMinuto*minutosPorHora*horasPorDia;
+document.write("<h1> calculadora del tiempo </h1><p> hay " + segundosPorDia + " en un dia </p>");*/
+
+
+console.log("Inicio del Programa");
+var peliculasVistas =prompt("Cuantas peliculas has visto en este mes");
+var seriesVistas= prompt("cuantas series has visto en este mes");
+var tiempoPantalla = parseInt(peliculasVistas)+ parseInt(seriesVistas);
+alert('Wow! has visto ' + tiempoPantalla + " veces minimo la pantalla.");
+console.log("fin del programa");
